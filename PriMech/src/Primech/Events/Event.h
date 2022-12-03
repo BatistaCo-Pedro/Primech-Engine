9b0141014dd2871d956d8f841ce104990843d178
Batista Pedro <pedrocorreiabatista@gmail.com>
2022-12-03 22:14:14 +0100

    added eventDispatcher

--- PriMech/src/Primech/Events/Event.h
+++ PriMech/src/Primech/Events/Event.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../Core.h"
+#include "PriMech/Core.h"
 
 #include <string>
 #include <functional>
@@ -42,17 +42,33 @@ namespace PriMech {
 			return GetCategoryFlags() & category;  //check the current category for the searched one
 		}
 	protected:
-		bool m_Handled = false; //was the event handled already?
+		bool m_Handled = false;
 	};
 
 	class EventDispatcher {
 		template<typename T>
-		using EventFN = std::function<bool(T&)>;
+		//returns bool and takes in T reference
+		//T can be any Event type
+		using EventFunction = std::function<bool(T&)>; 
 	public:
+		EventDispatcher(Event& event) : event_(event) {}
 
+		template<typename T>
+		bool Dispatch(EventFunction<T> func) {
+			//check if eventtype passed to constructor is equal to the eventtype of the function<T>
+			if (event_.GetEventType == T::GetStaticEventType()) {
+				//call function to determine if event was handled
+				event_.m_Handled = func(*(T*)&event_);
+				return true;
+			}
+			return false;
+		}
 	private:
-
-	protected:
-
+		Event& event_;
 	};
+
+	//used in the Logging library to easly call ToString() 
+	inline std::ostream& operator<< (std::ostream & os, const Event& e) {
+		return os << e.ToString();
+	}
 }
