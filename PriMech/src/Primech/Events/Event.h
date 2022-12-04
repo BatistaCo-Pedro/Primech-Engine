3a8990d64cf07392bee143aee82ca1ce15303dbb
Batista Pedro <pedrocorreiabatista@gmail.com>
2022-12-04 10:06:58 +0100

    .

--- PriMech/src/Primech/Events/Event.h
+++ PriMech/src/Primech/Events/Event.h
@@ -15,10 +15,10 @@ namespace PriMech {
 
 	enum EventCategory {
 		None = 0,
-		EventCategoryApplication = BIT(0),
+		EventCategoryApplication = BIT(0), 
 		EventCategoryInput = BIT(1),
 		EventCategoryKeyboard = BIT(2),
-		EventCategoryMouse= BIT(3),
+		EventCategoryMouse= BIT(3), 
 		EventCategoryMouseButton= BIT(4),
 	};
 
