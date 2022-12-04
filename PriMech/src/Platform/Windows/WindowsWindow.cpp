dba64a0b3e13844de88a99a626b6061f0a1f91a5
Batista Pedro <pedrocorreiabatista@gmail.com>
2022-12-04 16:00:26 +0100

    added event callback functions and debug print statements to application

--- PriMech/src/Platform/Windows/WindowsWindow.cpp
+++ PriMech/src/Platform/Windows/WindowsWindow.cpp
@@ -1,9 +1,16 @@
 #include "ppch.h"
 #include "Primech/Log.h"
 #include "WindowsWindow.h"
+#include <Primech/Events/ApplicationEvent.h>
+#include <Primech/Events/KeyEvent.h>
+#include <Primech/Events/MouseEvent.h>
 
 namespace PriMech{
 	static bool s_GLFWInitialized = false;
+
+	static void glfwErrorCallback(int error, const char* description) {
+		PM_CORE_ERROR("GLFW error ({0}): {1}", error, description);
+	}
 
 	Window* Window::Create(const WindowProps& props) {
 		return new WindowsWindow(props);
@@ -25,6 +32,7 @@ namespace PriMech{
 			//TODO terminate glfw on system shutdown
 			int success = glfwInit();
 			PM_CORE_ASSERT(scucces, "Could not initalize GLFW!");
+			glfwSetErrorCallback(glfwErrorCallback);
 
 			s_GLFWInitialized = true;
 		}
@@ -33,6 +41,74 @@ namespace PriMech{
 		glfwMakeContextCurrent(window_);
 		glfwSetWindowUserPointer(window_, &wData_);
 		SetVSync(true);
+
+		//Set GLFW callbacks , Lambda function --> []()
+		glfwSetWindowSizeCallback(window_, [](GLFWwindow* glfwWindow, int width, int height) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(glfwWindow);
+			WindowResizeEvent event(width, height);
+			data.width_ = width;
+			data.height_ = height;
+			data.EventCallback(event);
+		});
+
+		glfwSetWindowCloseCallback(window_, [](GLFWwindow* glfwWindow) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(glfwWindow);
+			WindowCloseEvent event;
+			data.EventCallback(event);
+		});
+
+		glfwSetKeyCallback(window_, [](GLFWwindow* glfwWindow, int key, int scancode, int action, int mods) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(glfwWindow);
+			
+			switch (action)
+			{
+				case GLFW_PRESS: {
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE: {
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_REPEAT: {
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetMouseButtonCallback(window_, [](GLFWwindow* glfwWindow, int button, int action, int mods) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(glfwWindow);
+			
+			switch (action)
+			{
+				case GLFW_PRESS: {
+					MouseButtonPressedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE: {
+					MouseButtonReleasedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetScrollCallback(window_, [](GLFWwindow* glfwWindow, double xOffset, double yOffset) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(glfwWindow);
+			MouseScrolledEvent event((float)xOffset, (float)yOffset);
+			data.EventCallback(event);
+		});
+
+		glfwSetCursorPosCallback(window_, [](GLFWwindow* glfwWindow, double xPos, double yPos) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(glfwWindow);
+			MouseMovedEvent event((float)xPos, (float)yPos);
+			data.EventCallback(event);
+		});
 	}
 
 	void WindowsWindow::ShutDown() {
--- PriMech/src/Primech/Application.cpp
+++ PriMech/src/Primech/Application.cpp
@@ -4,12 +4,27 @@
 
 namespace PriMech {
 	
+#define BIND_EVENT_FUNCTION(x)	std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application() {
 		pWindow_ = std::unique_ptr<Window>(Window::Create());
+		pWindow_->SetEventCallback(BIND_EVENT_FUNCTION(Application::OnEvent));
 	}
 
-	Application::~Application() {
+	Application::~Application() {}
 
+	void Application::OnEvent(Event& event) {
+		EventDispatcher dispatcher(event);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FUNCTION(Application::OnWindowClose));
+
+		//Printign all events for debugging purposes
+		if (event.IsInCategory(EventCategoryApplication) ||
+			event.IsInCategory(EventCategoryKeyboard)) {
+			PM_CORE_TRACE(event); //keyboard and window events in white
+		}
+		else {
+			PM_CORE_DEBUG(event); //mouse events in blue
+		}
 	}
 
 	void Application::Run() {
@@ -24,5 +39,10 @@ namespace PriMech {
 		while (running_) {
 			pWindow_->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& event) {
+		running_ = false;
+		return true;
 	}
 }
